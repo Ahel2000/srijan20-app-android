aa30c1a9643e3308f307d6fad926502fefa88086
Rishav Agarwal <rishavcoder@gmail.com>
2020-02-19 23:36:33 +0530

    update: sponsors

--- app/src/main/java/in/srijanju/androidapp/view/Sponsors.java
+++ app/src/main/java/in/srijanju/androidapp/view/Sponsors.java
@@ -1,17 +1,18 @@
 package in.srijanju.androidapp.view;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
@@ -26,17 +27,16 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 import in.srijanju.androidapp.R;
-import in.srijanju.androidapp.SrijanActivity;
 import in.srijanju.androidapp.controller.SponsorAdapter;
 import in.srijanju.androidapp.model.Sponsor;
 
 public class Sponsors extends Fragment {
 
-  final ArrayList<Sponsor> sponsors = new ArrayList<>();
-  final SponsorAdapter adapter = new SponsorAdapter(getActivity(), sponsors);
-  DatabaseReference ref = null, checkRef = null;
+  private final ArrayList<Sponsor> sponsors = new ArrayList<>();
+  private SponsorAdapter adapter;
+  private DatabaseReference ref = null, checkRef = null;
 
-  ChildEventListener eventListener = new ChildEventListener() {
+  private ChildEventListener eventListener = new ChildEventListener() {
 	@Override
 	public void onChildAdded(
 			@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
@@ -71,7 +71,7 @@ public class Sponsors extends Fragment {
   };
 
   private boolean isSponsored = false;
-  ValueEventListener sponsoredValueListener = new ValueEventListener() {
+  private ValueEventListener sponsoredValueListener = new ValueEventListener() {
 	@Override
 	public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 	  Boolean isSpon = null;
@@ -93,39 +93,64 @@ public class Sponsors extends Fragment {
 	public void onCancelled(@NonNull DatabaseError databaseError) {
 	}
   };
-	@Override
-	public void onAttach(Context context) {
-		super.onAttach(context);
-	}
 
-	@Nullable
-	@Override
-	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.activity_sponsors, container, false);
-	}
+  @Nullable
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+	return inflater.inflate(R.layout.activity_sponsors, container, false);
+  }
+
   @Override
   public void onActivityCreated(Bundle savedInstanceState) {
 	super.onActivityCreated(savedInstanceState);
 
+	final FragmentActivity activity = getActivity();
+	View view = getView();
+	if (activity == null || view == null) {
+	  Toast.makeText(getContext(), "Some error occurred", Toast.LENGTH_SHORT).show();
+	  return;
+	}
 
 	FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 	if (user == null) {
-	  Toast.makeText(getActivity(), "Not logged in", Toast.LENGTH_SHORT).show();
+	  Toast.makeText(activity, "Not logged in", Toast.LENGTH_SHORT).show();
 	  FirebaseAuth.getInstance().signOut();
-	  AuthUI.getInstance().signOut(getContext());
-	  Intent intent = new Intent(getActivity(), MainActivity.class);
+	  AuthUI.getInstance().signOut(activity.getApplicationContext());
+	  Intent intent = new Intent(activity, MainActivity.class);
 	  intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
 	  startActivity(intent);
 	  return;
 	}
 
+	adapter = new SponsorAdapter(activity, sponsors);
+
 	ListView lvSponsors = getView().findViewById(R.id.lv_sponsors);
 	lvSponsors.setAdapter(adapter);
 	lvSponsors.setEmptyView(getView().findViewById(R.id.tv_no_sponsor));
 
+	final TextView tvSponsorText = getView().findViewById(R.id.tv_sponsor_text);
+
 	FirebaseDatabase db = FirebaseDatabase.getInstance();
 	ref = db.getReference("srijan/sponsors");
 	checkRef = db.getReference("srijan/sponsors/isSponsored");
+	db.getReference("srijan/sponsors/askSponsor").addListenerForSingleValueEvent(new ValueEventListener() {
+	  @Override
+	  public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+		Boolean askSpon = null;
+		if (dataSnapshot.exists()) askSpon = dataSnapshot.getValue(Boolean.class);
+		if (askSpon == null || !askSpon) {
+		  tvSponsorText.setVisibility(View.GONE);
+		  return;
+		}
+
+		tvSponsorText.setVisibility(View.VISIBLE);
+	  }
+
+	  @Override
+	  public void onCancelled(@NonNull DatabaseError databaseError) {
+		tvSponsorText.setVisibility(View.GONE);
+	  }
+	});
   }
 
 
